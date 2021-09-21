23fc49e1603f7d78eefcb0089a95a3aaeacc510c
friel2021 <friel2009@gmail.com>
2021-09-21 13:40:20 -0600

    added routes

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const router = require("./routes/api.js");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+let db = mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/Workout-Tracker",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
+
+app.use(router);
+require("./routes/public.js")(app);
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
+
+module.exports = db;
